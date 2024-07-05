4a695e2d962b00833d538b4d9ae8a7bb87bf31be
Samyar <samyarkd@gmail.com>
2024-07-05 23:53:55 +0330

    idk

--- lua/custom/plugins/toggle-term.lua
+++ lua/custom/plugins/toggle-term.lua
@@ -1,74 +1,75 @@
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
-  opts = {
-    open_mapping = [[<c-\>]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
-  },
+  event = 'VeryLazy',
   config = function()
+    -- custom setup
+    require('toggleterm').setup {
+      direction = 'float', -- float|horizontal|vertical|tab
+      float_opts = {
+        border = 'curved', -- single|double|curved|shadow
+        -- winblend = 5, -- transparency, recommended: 5
+      },
+      persist_mode = true,
+      highlights = {
+        Normal = { link = 'ToogleTermNormal' },
+        NormalFloat = { link = 'ToogleTermNormalFloat' },
+        FloatBorder = { link = 'ToggleTermFloatBorder' },
+      },
+      on_create = function()
+        -- disable sign column (annoying black space at the left)
+        vim.cmd [[ setlocal signcolumn=no ]]
+      end,
+    }
+
+    -- require toggleterm:terminal instance
+    local toggleterm_terminal = require 'toggleterm.terminal'
+
+    -- general toggleterm terminal instance
+    local Terminal = toggleterm_terminal.Terminal
+
+    -- main terminal
+    local main_term = Terminal:new {
+      display_name = ' TERMINAL ',
+      on_open = function()
+        vim.cmd [[ startinsert! ]]
+      end,
+    }
+
+    -- secondary terminal
+    local secondary_term = Terminal:new {
+      -- cmd = "<cmd>",
+      -- dir = "~/",
+      display_name = 'secondary terminal',
+      hidden = false,
+      direction = 'horizontal',
+      size = 18,
+      on_open = function()
+        vim.cmd [[ startinsert! ]]
+      end,
+    }
+
+    -- global: main term toggle
+    function _G.main_term_toggle()
+      main_term:toggle()
+    end
+
+    -- global: secondary term toggle
+    function _G.secondary_term_toggle()
+      secondary_term:toggle()
+    end
+
+    -- global: attach general key shortcuts
     function _G.set_terminal_keymaps()
       local opts = { buffer = 0 }
       vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-      vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-      vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-      vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-      vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-      vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
     end
 
-    -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-    vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
-
-    local Terminal = require('toggleterm.terminal').Terminal
-
-    local terminals = {}
-
-    -- Detect the operating system
-    local is_windows = vim.loop.os_uname().sysname == 'Windows_NT'
-    local shell_cmd = is_windows and 'pwsh' or 'bash'
-
-    local function create_terminal(cmd, keymap, desc)
-      local term = Terminal:new {
-        cmd = cmd,
-        direction = 'float',
-        float_opts = {
-          border = 'double',
-        },
-        on_open = function(term)
-          vim.cmd 'startinsert!'
-          vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
-        end,
-        on_close = function()
-          vim.cmd 'startinsert!'
-        end,
-      }
-
-      local function toggle_terminal()
-        -- Close all other terminals
-        for _, t in pairs(terminals) do
-          if t ~= term and t:is_open() then
-            t:close()
-          end
-        end
-        -- Toggle the current terminal
-        term:toggle()
-      end
-
-      -- Ensure the global function is unique by using a unique identifier
-      local function_name = desc .. '_toggle'
-      _G[function_name] = toggle_terminal
-
-      vim.api.nvim_set_keymap('n', keymap, '<cmd>lua _G.' .. function_name .. '()<CR>', { noremap = true, silent = true, desc = desc })
-      -- Add the terminal to the list of terminals
-      table.insert(terminals, term)
-
-      return term
-    end
-
-    -- Create terminals
-    create_terminal('lazygit', '<leader>gg', 'lazygit')
-    -- Create terminals with the appropriate shell command
-    create_terminal(shell_cmd, '<A-1>', 'shellone')
-    create_terminal(shell_cmd, '<A-2>', 'shelltwo')
-    create_terminal(shell_cmd, '<A-3>', 'shellthree')
+    -- set global toggleterm keymaps
+    vim.cmd [[ autocmd! TermOpen term://* lua set_terminal_keymaps() ]]
   end,
+  keys = {
+    { '<A-1>', '<cmd>lua main_term_toggle();<cr>', desc = 'Toggle Tree' },
+    { '<A-2>', '<cmd>lua secondary_term_toggle();<cr>', desc = 'Toggle Tree' },
+  },
 }
