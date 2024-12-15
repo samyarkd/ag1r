6b22b294df3dd6e4539bafc6ec364b1df10eacf2
samyar <samyarkd@gmail.com>
2024-12-15 16:47:35 +0330

    fix: test

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,32 +1,7 @@
-local inlayHints = {
-  includeInlayParameterNameHints = "all",
-  includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-  includeInlayFunctionParameterTypeHints = true,
-  includeInlayVariableTypeHints = true,
-  includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-  includeInlayPropertyDeclarationTypeHints = true,
-  includeInlayFunctionLikeReturnTypeHints = true,
-  includeInlayEnumMemberValueHints = true,
-}
-
 ---@type lspconfig.Config
-local opts = {
-  settings = {
-    typescript = {
-      inlayHints = inlayHints,
-    },
-    javascript = {
-      inlayHints = inlayHints,
-    },
-  },
-}
+local opts = {}
 
 return {
-  {
-    "chrisgrieser/nvim-lsp-endhints",
-    event = "LspAttach",
-    opts = {}, -- required, even if empty
-  },
   {
     "neovim/nvim-lspconfig",
     opts = opts,
