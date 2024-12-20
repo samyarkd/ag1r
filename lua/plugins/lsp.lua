ce46641474502ffd336af9b4860b85eeb98b67c3
samyar <samyarkd@gmail.com>
2024-12-20 14:07:35 +0330

    set up avante

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,18 @@
+local inlayHints = {
+  includeInlayParameterNameHints = "all",
+  includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+  includeInlayFunctionParameterTypeHints = true,
+  includeInlayVariableTypeHints = true,
+  includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+  includeInlayPropertyDeclarationTypeHints = true,
+  includeInlayFunctionLikeReturnTypeHints = true,
+  includeInlayEnumMemberValueHints = true,
+}
+
 ---@type lspconfig.Config
-local opts = {}
+local opts = {
+  inlay_hints = { enabled = false },
+}
 
 return {
   {
