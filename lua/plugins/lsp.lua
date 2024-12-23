cdfda9392de0186f98d7f6c4eabb8deb15ca61df
samyar <samyarkd@gmail.com>
2024-12-23 23:07:02 +0330

    cmp-cmdline

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,5 +18,33 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = opts,
+    config = function()
+      local cmp = require("cmp")
+      -- `/` cmdline setup.
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      -- `:` cmdline setup.
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" },
+            },
+          },
+        }),
+      })
+    end,
+    dependencies = {
+      { "hrsh7th/cmp-cmdline" },
+    },
   },
 }
