97a42fc9f254dae6e08c293d9f74ccaf1ac72202
samyar <samyarkd@gmail.com>
2025-01-01 13:25:43 +0330

    fix: update

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins require("config.lazy")
 require("config.lazy")
+
 -- Enable spell check
 vim.cmd("set spell")
 vim.cmd("colorscheme carbonfox")
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,15 +1,3 @@
-local inlayHints = {
-  includeInlayParameterNameHints = "all",
-  includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-  includeInlayFunctionParameterTypeHints = true,
-  includeInlayVariableTypeHints = true,
-  includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-  includeInlayPropertyDeclarationTypeHints = true,
-  includeInlayFunctionLikeReturnTypeHints = true,
-  includeInlayEnumMemberValueHints = true,
-}
-
----@type lspconfig.Config
 local opts = {
   inlay_hints = { enabled = false },
 }
@@ -18,31 +6,6 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = opts,
-    config = function()
-      local cmp = require("cmp")
-      -- `/` cmdline setup.
-      cmp.setup.cmdline("/", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
-
-      -- `:` cmdline setup.
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          {
-            name = "cmdline",
-            option = {
-              ignore_cmds = { "Man", "!" },
-            },
-          },
-        }),
-      })
-    end,
     dependencies = {
       { "hrsh7th/cmp-cmdline" },
     },
